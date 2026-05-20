73e7af95b40d4add950c1364b4c026dbae80406f
Ratan Kumar <krratan80@gmail.com>
2026-05-20 12:38:29 +0530

    feat: add Vercel Analytics and Speed Insights for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Providers } from "./providers";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
@@ -58,6 +59,8 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics/>
+        <SpeedInsights />
       </body>
     </html>
   );
